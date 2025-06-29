3de23f9849eb516c9f253b1c4c62989501130d3a
s-zhdanova <sophia.m.zhdanova@gmail.com>
2025-06-29 10:43:43 -0400

    Menu, header with logo

---
+++ menu.js
@@ -0,0 +1,31 @@
+const homeButton = document.getElementById("homeButton");
+homeButton.addEventListener("mouseenter", () => {
+    homeButton.style.backgroundColor = "#d98f8f";
+});
+homeButton.addEventListener("mouseleave", () => {
+    homeButton.style.backgroundColor = "#bc6e6e";
+});
+
+const aboutButton = document.getElementById("aboutButton");
+aboutButton.addEventListener("mouseenter", () => {
+    aboutButton.style.backgroundColor = "#d98f8f";
+});
+aboutButton.addEventListener("mouseleave", () => {
+    aboutButton.style.backgroundColor = "#bc6e6e";
+});
+
+const contactButton = document.getElementById("contactButton");
+contactButton.addEventListener("mouseenter", () => {
+    contactButton.style.backgroundColor = "#d98f8f";
+});
+contactButton.addEventListener("mouseleave", () => {
+    contactButton.style.backgroundColor = "#bc6e6e";
+});
+
+const articlesButton = document.getElementById("articlesButton");
+articlesButton.addEventListener("mouseenter", () => {
+    articlesButton.style.backgroundColor = "#d98f8f";
+});
+articlesButton.addEventListener("mouseleave", () => {
+    articlesButton.style.backgroundColor = "#bc6e6e";
+});
